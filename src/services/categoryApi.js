a45e1843ed3a5deb109cad483ffe4b1213d3630b
renuka-deshmukh <renuka.deshmukh776@gmail.com>
2025-10-09 14:41:05 +0530

    updated

--- src/services/categoryApi.js
+++ src/services/categoryApi.js
@@ -2,8 +2,13 @@ import axiosInstance from "./api";
 
 export const getAllCategories = () => axiosInstance.get('/category/getAllCategories');
 
-export const createCategory = (data) => axiosInstance.post('/category/createCategory', data);
+export const createCategory = (formData) => 
+    axiosInstance.post('/category/createCategory', formData,{
+     headers: { "Content-Type": "multipart/form-data" },
+});
 
 export const deleteCategory = (id) => axiosInstance.delete(`/category/deleteCategory/${id}`);
 
-export const updateCategory = (id, formData) => axiosInstance.put(`/category/updateCategory/${id}`, formData)
+export const updateCategory = (id, formData) => axiosInstance.put(`/category/updateCategory/${id}`, formData,{
+    headers: { "Content-Type": "multipart/form-data" },
+});
